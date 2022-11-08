1a0659b61b5c8d030bedb8b667dc22dd9152bcdb
Mario David <mario@die-davids.de>
2022-11-08 08:47:51 +0100

    default name for customer header label

--- src/main/java/io/jmix/bookstore/customer/screen/CustomerEdit.java
+++ src/main/java/io/jmix/bookstore/customer/screen/CustomerEdit.java
@@ -1,13 +1,24 @@
 package io.jmix.bookstore.customer.screen;
 
 import io.jmix.bookstore.customer.Customer;
-import io.jmix.ui.screen.EditedEntityContainer;
-import io.jmix.ui.screen.StandardEditor;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiDescriptor;
+import io.jmix.ui.component.HasValue;
+import io.jmix.ui.component.Label;
+import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 
 @UiController("bookstore_Customer.edit")
 @UiDescriptor("customer-edit.xml")
 @EditedEntityContainer("customerDc")
 public class CustomerEdit extends StandardEditor<Customer> {
+    @Autowired
+    private Label<String> customerNameHeaderLabel;
+    private final String DEFAULT_UNKNOWN_CUSTOMER_HEADER_LABEL = "Unknown Customer \uD83D\uDC71";
+
+    @Subscribe("customerNameHeaderLabel")
+    public void onCustomerNameHeaderLabelValueChange(HasValue.ValueChangeEvent<String> event) {
+        if (!StringUtils.hasText(event.getValue())) {
+            customerNameHeaderLabel.setValue(DEFAULT_UNKNOWN_CUSTOMER_HEADER_LABEL);
+        }
+    }
 }
