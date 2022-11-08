3fe2d3ce23db7fecf777596f5c2a9ea0b5c14904
Mario David <mario@die-davids.de>
2022-11-08 07:47:29 +0100

    remove unused code

--- src/main/java/io/jmix/bookstore/product/supplier/screen/supplierorder/SupplierOrderBrowse.java
+++ src/main/java/io/jmix/bookstore/product/supplier/screen/supplierorder/SupplierOrderBrowse.java
@@ -1,19 +1,11 @@
 package io.jmix.bookstore.product.supplier.screen.supplierorder;
 
 import io.jmix.bookstore.product.supplier.PerformSupplierOrderService;
-import io.jmix.ui.Notifications;
-import io.jmix.ui.action.Action;
-import io.jmix.ui.component.DataGrid;
-import io.jmix.ui.component.NotificationFacet;
-import io.jmix.ui.download.Downloader;
-import io.jmix.ui.screen.*;
 import io.jmix.bookstore.product.supplier.SupplierOrder;
-import org.flowable.engine.RuntimeService;
+import io.jmix.ui.action.Action;
+import io.jmix.ui.component.NotificationFacet;
+import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 @UiController("bookstore_SupplierOrder.browse")
 @UiDescriptor("supplier-order-browse.xml")
@@ -23,9 +15,6 @@ public class SupplierOrderBrowse extends StandardLookup<SupplierOrder> {
     private PerformSupplierOrderService performSupplierOrderService;
     @Autowired
     private NotificationFacet supplierOrdersFromRequestsCreatedConfirmation;
-
-    @Autowired
-    private Downloader downloader;
 
     @Subscribe("supplierOrdersTable.createSupplierOrdersFromRequests")
     public void onSupplierOrdersTableCreateSupplierOrdersFromRequests(Action.ActionPerformedEvent event) {
