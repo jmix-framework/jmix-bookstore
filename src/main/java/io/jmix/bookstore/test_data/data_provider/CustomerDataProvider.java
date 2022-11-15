c66b74ac23dd389db478638c91a7cc8206f833de
Mario David <mario@die-davids.de>
2022-11-15 08:31:35 +0100

    remove unsed code

--- src/main/java/io/jmix/bookstore/test_data/data_provider/CustomerDataProvider.java
+++ src/main/java/io/jmix/bookstore/test_data/data_provider/CustomerDataProvider.java
@@ -3,7 +3,6 @@ package io.jmix.bookstore.test_data.data_provider;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jmix.bookstore.customer.Customer;
-import io.jmix.bookstore.product.Product;
 import io.jmix.core.DataManager;
 import io.jmix.core.SaveContext;
 import net.datafaker.*;
@@ -15,23 +14,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static io.jmix.bookstore.test_data.data_provider.RandomValues.randomOfList;
 
 @Component("bookstore_CustomerDataProvider")
 public class CustomerDataProvider implements TestDataProvider<Customer, CustomerDataProvider.DataContext> {
 
-    private final Logger log = LoggerFactory.getLogger(CustomerDataProvider.class);
 
     protected final DataManager dataManager;
     protected final ResourceLoader resourceLoader;
@@ -82,13 +76,6 @@ public class CustomerDataProvider implements TestDataProvider<Customer, Customer
                 .values().stream()
                 .map(customers -> customers.get(0))
                 .collect(Collectors.toList());
-
-//        return Stream.generate(faker::book).limit(amount)
-//                .map(book -> toProduct(book, productCategories, suppliers))
-//                .collect(Collectors.groupingBy(Product::getName))
-//                .values().stream()
-//                .map(products -> products.get(0))
-//                .collect(Collectors.toList());
     }
 
     private AddressEntries addresses(String addressesFileName) {
@@ -152,18 +139,7 @@ public class CustomerDataProvider implements TestDataProvider<Customer, Customer
         addressEntity.setPostCode(address.postalCode());
         Point point = new GeometryFactory().createPoint(new Coordinate(address.coordinates().lng(), address.coordinates().lat()));
         addressEntity.setPosition(point);
-//        Optional<AddressLocation> addressLocation = getAddressLocation(address);
 
-//        try {
-//            Thread.sleep(2000);
-//            addressLocation.ifPresent(it -> {
-//                Point point = new GeometryFactory().createPoint(new Coordinate(it.lon(), it.lat()));
-//                addressEntity.setPosition(point);
-//            });
-//
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
         return addressEntity;
     }
 
@@ -174,35 +150,5 @@ public class CustomerDataProvider implements TestDataProvider<Customer, Customer
 
         return entities;
     }
-
-//
-//    public Optional<AddressLocation> getAddressLocation(AddressEntry addressEntry) {
-//        try {
-//            AddressLocation[] response = restTemplate.getForObject("https://us1.locationiq.com/v1/search.php" +
-//                            "?key=pk.20ee504d36d6f478cfcfb100da13d68f" +
-//                            "&street={street}" +
-//                            "&postalcode={postalcode}" +
-//                            "&state={state}" +
-//                            "&format=json",
-//                    AddressLocation[].class,
-//                    addressEntry.address(), addressEntry.zip(), addressEntry.state());
-//
-//            List<AddressLocation> addressLocations = Arrays.asList(response);
-//
-//            log.info("Result for '{}': {}", addressEntry, addressLocations);
-//
-//            if (!CollectionUtils.isEmpty(addressLocations)) {
-//                return Optional.ofNullable(addressLocations.get(0));
-//            }
-//
-//            return Optional.empty();
-//        } catch (Exception e) {
-//            log.warn("Error getting Location information for Address '{}': {}", addressEntry, e);
-//            return Optional.empty();
-//        }
-//    }
-
-//    record AddressLocation(double lat, double lon) {
-//    }
 
 }
